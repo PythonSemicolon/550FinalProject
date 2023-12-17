a4883acab9f9bab6f76fb2fb6d422af97c2a9813
Alberto Lopez Garcia <albertojoel.lg@gmail.com>
2023-12-16 20:47:05 -0600

    Add Outline for main.py
    Added this file for reference purposes

---
+++ main.py
@@ -0,0 +1,25 @@
+# For every translator
+
+    # For every work of the translator
+
+        # Load work
+
+        # Perform stylometric analysis on each fragment with winSize = win_length
+
+        # Save in X_train_translator, y_train_translator
+
+    # Train linear classifier
+
+    # Load test work
+
+    # Perform stylometric analysis on each fragment with winSize = win_length
+
+    # Save in X_test_translator, y_test_translator
+
+    # Predict
+
+    # Save results in results_translator
+
+
+# TODO: Write function 'get_stylometric_features'
+# Note: Updated stylometric_analyzer.py to shorten the class
